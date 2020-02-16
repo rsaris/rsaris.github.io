1777a3ce69b13522cc3e16af47c028394f0c75a7
Bob Saris <bob@bobsaris.com>
2020-02-15 21:39:46 -0500

    Update style and content of about page.

--- src/about_page/index.jsx
+++ src/about_page/index.jsx
@@ -4,7 +4,23 @@ import './styles.scss';
 
 const AboutPage = () => (
   <div className="AboutPage padded-page">
-    <h1>I am Bob</h1>
+    <h1>Bob Saris</h1>
+    <div className="AboutPage__contact">
+      bob at bobsaris.com<br />
+      <a href="https://www.linkedin.com/in/robertsaris/">LinkedIn</a>
+    </div>
+    <p>
+      Bob is the CTO and Co-Founder of User Interviews (
+      <a href="https://www.userinterviews.com/">
+        www.userinterviews.com
+      </a>
+      ). He graduated from Carnegie Mellon University in May 2010 with a
+      Bachelors in Computer Science.
+    </p>
+    <p>
+      Outside of work he enjoys playing video games, baking bread, knitting, and
+      spending time with his wife and son.
+    </p>
   </div>
 );
 
